8fe19d8caa2514f5218198be2619815dad21975c
michal.ziolecki <michal.ziolecki@op.pl>
2020-05-16 08:33:50 +0200

    Ex3 added

---
+++ src/main/java/java_exercises_1_sda/Ex3.java
@@ -0,0 +1,7 @@
+package java_exercises_1_sda;
+
+public class Ex3 {
+    public int mulitplyTwoNumber(int a, int b){
+        return a * b ;
+    }
+}
--- src/main/java/java_exercises_1_sda/Main.java
+++ src/main/java/java_exercises_1_sda/Main.java
@@ -3,10 +3,17 @@ package java_exercises_1_sda;
 public class Main {
 
     public static void main(String[] args){
+
         Ex1.printMultiplying(5);
+
         boolean result = Ex2.isPrimeNumber(7); // true
         System.out.println("isPrime: " + result);
         boolean resultTwo = Ex2.isPrimeNumber(8); // false
         System.out.println("isPrime: " + resultTwo);
+
+        Ex3 objectEx1 = new Ex3();
+        int ret = objectEx1.mulitplyTwoNumber(4, 5);
+        System.out.println("Multiplying: " + ret);
+        
     }
 }
