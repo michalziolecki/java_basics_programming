bace2cba6e144e124e50b3cfb799fcbb8586d049
michal.ziolecki <michal.ziolecki@op.pl>
2020-05-16 08:28:25 +0200

    Exercises 1 and 2

---
+++ src/main/java/java_exercises_1_sda/Ex1.java
@@ -0,0 +1,10 @@
+package java_exercises_1_sda;
+
+public class Ex1 {
+    public static void printMultiplying(int number){
+        for (int i=1; i < 5 ; i++){
+            int result = number * i;
+            System.out.println(result);
+        }
+    }
+}
---
+++ src/main/java/java_exercises_1_sda/Main.java
@@ -0,0 +1,12 @@
+package java_exercises_1_sda;
+
+public class Main {
+
+    public static void main(String[] args){
+        Ex1.printMultiplying(5);
+        boolean result = Ex2.isPrimeNumber(7); // true
+        System.out.println("isPrime: " + result);
+        boolean resultTwo = Ex2.isPrimeNumber(8); // false
+        System.out.println("isPrime: " + resultTwo);
+    }
+}
