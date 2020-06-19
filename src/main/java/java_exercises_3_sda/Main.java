df772d52196c97ee9555e969b5f9c2cc1251c1ac
michal.ziolecki <michal.ziolecki@op.pl>
2020-06-19 22:40:53 +0200

    przyklad 13 w main

--- src/main/java/java_exercises_3_sda/Main.java
+++ src/main/java/java_exercises_3_sda/Main.java
@@ -1,5 +1,10 @@
 package java_exercises_3_sda;
 
+import java_exercises_3_sda.exFrom11To13.Ex13;
+import java_exercises_3_sda.exFrom11To13.Figure;
+import java_exercises_3_sda.exFrom11To13.Rectangle;
+import java_exercises_3_sda.exFrom11To13.Triangle;
+
 public class Main {
 
     public static void main(String[] args){
@@ -9,6 +14,12 @@ public class Main {
         //Ex5
         Ex5.ex5();
         System.out.println("======");
-        //ex6
+
+
+        //ex13
+        Rectangle rectangle = new Rectangle(2.0, 2.0);
+        Triangle triangle = new Triangle(2.0, 2.0);
+        Figure[] figures = {rectangle, triangle};
+        System.out.println(Ex13.isEnought(figures, 8.0));
     }
 }
