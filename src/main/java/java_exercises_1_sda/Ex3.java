24a5fe877c3bf105e1ee9374d09c037f2a67d3f1
michal.ziolecki <michal.ziolecki@op.pl>
2020-11-07 13:03:54 +0100

    .

--- src/main/java/java_exercises_1_sda/Ex3.java
+++ src/main/java/java_exercises_1_sda/Ex3.java
@@ -1,7 +1,7 @@
 package java_exercises_1_sda;
 
 public class Ex3 {
-    public int multiplyTwoNumber(int a, int b){
+    public int sumNumbersFromaToB(int a, int b){
         int sum = 0;
         for (int i = a; i <= b; i++) {
             sum += i;
