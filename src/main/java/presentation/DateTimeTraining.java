6e91ec18277c1166c35c22d3912ba6124142a315
michal.ziolecki <michal.ziolecki@op.pl>
2020-12-05 09:41:18 +0100

    drugi testowy commit

--- src/main/java/presentation/DateTimeTraining.java
+++ src/main/java/presentation/DateTimeTraining.java
@@ -6,30 +6,43 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
-public class DateTimeTraining {
+public class DateTimeTraining{
 
-    public static void  dateNow(){
+    private String color = "Red";
+
+    // metoda wykorzystujaca niestatyczne atrybuty klasy musi byc nie statyczna
+    // czyli na obiekcie zawołana
+    void dateNowTwo(){
+        String hereColor = this.color;
+        double pi = Math.PI;
+    }
+
+    static void  dateNow(){
         LocalDateTime ldt = LocalDateTime.now();
         System.out.println("LocalDateTime: " + ldt);
         Instant now = Instant.now();
         System.out.println("Instant: " + now);
     }
 
-    public static void tokyoDateTime(){
+    static void tokyoDateTime(){
         // Strefy dokumentacja https://garygregory.wordpress.com/2013/06/18/what-are-the-java-timezone-ids/
         Clock clock = Clock.system(ZoneId.of("Asia/Tokyo"));
         LocalDateTime ldt = LocalDateTime.now(clock);
         System.out.println("Japan time:" + ldt);
     }
 
-    public static void dateFormat(){
+    static void dateFormat(){
         LocalDateTime ldt = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/mm/dd");
         System.out.println("Date in format yyyy/mm/dd: " + formatter.format(ldt));
     }
 
     public static void main(String[] args){
-        dateNow();
+        DateTimeTraining dateTimeTraining = new DateTimeTraining();
+        // metoda nie statyczna musi byc na obiekcie
+        dateTimeTraining.dateNowTwo();
+        // metoda statyczna nie musi miec obiektu, moze byc wolana na klasie
+        dateNow(); // == DateTimeTraining.dateNow();
         tokyoDateTime();
         dateFormat();
     }
